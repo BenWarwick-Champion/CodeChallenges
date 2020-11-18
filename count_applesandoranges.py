ed5d7e2bb623b93261103de25dc74afb13204a1b
Ben Warwick-Champion <ben.w-c@sky.com>
2020-11-18 22:11:17 +0000

    Added a super pythonic method of solving this problem

--- count_applesandoranges.py
+++ count_applesandoranges.py
@@ -1,6 +1,7 @@
 # https://www.hackerrank.com/challenges/apple-and-orange/problem?h_r=next-challenge&h_v=zen
 
 
+# My solution
 def count_applesandoranges(s, t, a, b, apples, oranges):
     apple_counter, orange_counter = 0, 0
     for apple in apples:
@@ -9,10 +10,18 @@ def count_applesandoranges(s, t, a, b, apples, oranges):
     for orange in oranges:
         if (b + orange) >= s and (b + orange) <= t:
             orange_counter += 1
-            
+
     print(apple_counter)
     print(orange_counter)
 
+
+# Ultra pythonic solution -- navjotahuja92 (Hackerrank)
+def count_applesandoranges2(s, t, a, b, apples, oranges):
+    print(sum([1 for x in apples if (x + a) >= s and (x + a) <= t]))
+    print(sum([1 for x in oranges if (x + b) >= s and (x + b) <= t]))
+
+
+# This is boilerplate input code from HackerRank
 if __name__ == '__main__':
     st = input().split()
     s = int(st[0])
