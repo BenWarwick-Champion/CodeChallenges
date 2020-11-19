ba8440b9c83b35a902134ca52a2145a68ba59b31
Ben Warwick-Champion <ben.w-c@sky.com>
2020-11-19 15:24:22 +0000

    Added function for generating a given number of primes

--- prime_numbers.py
+++ prime_numbers.py
@@ -29,7 +29,27 @@ def sieve_primes(num):
             for i in range(prime, num + 1, prime):
                 sieve[i] = False
 
+# Iterative version of prime generation
+def gen_primes():
+# Sieve of Eratosthenes
+# Code by David Eppstein, UC Irvine, 28 Feb 2002
+# http://code.activestate.com/recipes/117119/
+    D = {}
+    q = 2
+    while True:
+        if q not in D:
+            yield q
+            D[q * q] = [q]
+        else:
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+        q += 1
+
 if __name__ == "__main__":
     input_number = input('Enter the range: ')
-
     sieve_primes(int(input_number))
+    print("------End Sequence------")
+
+    for index, n in zip(range(100), gen_primes()):
+        print(index, n)
